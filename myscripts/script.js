f570c153d90c56be378255839cebbf5253b3cc72
martinbarge <m.i.barge@qmul.ac.uk>
2019-08-31 19:15:12 +0100

    Update script.js

--- myscripts/script.js
+++ myscripts/script.js
@@ -1,21 +1,25 @@
 var idioms = [
-'Once in a blue moon = very rarely.', 
+'Once in a blue moon = Very rarely.', 
 
-'Tickled pink = to be extremely pleased.', 
+'Tickled pink = To be extremely pleased.', 
 
-'Caught red-handed = to catch someone in the act of doing something.',
+'Caught red-handed = To catch someone in the act of doing something.',
     
-'White lie = a small lie that is told to be polite or avoid hurting someone’s feelings.'
+'A white lie = A small lie that is told to be polite or avoid hurting someone’s feelings.',
+    
+'Out of the blue = Unexpectedly.'
 ];
 
 var examples = [
-'Example: <i>Once in a blue moon you will see that mean professor smile.</i>', 
+'Example: <i>Once in a blue moon you might see a solar eclipse.</i>', 
 
 'Example: <i>Your grandma was tickled pink that you called on her birthday!</i>', 
 
 'Example: <i>He was caught red-handed while stealing those biscuits.</i>',
     
-'Example: <i>I didn’t like her dress, but I told a white lie because I didn’t want to offend her.</i>'
+'Example: <i>I didn’t like her dress, but I told a white lie because I didn’t want to offend her.</i>',
+ 
+'Example: <i>My sister phoned me out of the blue last week. She hadn\'t phoned for ages.</i>'
 ];
 
 function loadQuote() {
